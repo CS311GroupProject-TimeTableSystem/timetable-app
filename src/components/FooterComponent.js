cf6e580809d094a400b84eb9b97272a90c29f6b8
s16417 <s16417@sci.pdn.ac.lk>
2020-07-08 22:12:54 +0530

    minor changes

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -6,7 +6,7 @@ function Footer(props) {
         <div className="footer">
             <div className="container">
                 <div className="row justify-content-center">             
-                    <div className="col-4 offset-1 col-sm-2">
+                    <div className="col-4 col-sm-3">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
                             <li><Link style={{ textDecoration: 'none' }} to="/home" href="#">Home</Link></li>
@@ -15,23 +15,17 @@ function Footer(props) {
                             <li><Link style={{ textDecoration: 'none' }} to="/help" href="#">Help</Link></li>
                         </ul>
                     </div>
-                    <div className="col-7 col-sm-5">
+                    <div className="col-7 col-sm-9">
                         <h5>Address</h5>
                         <address id="addr">
                         CP/G Weera Wijaya Wimalarathana M.V.,<br />
-                        Sigiriya Road<br />
-                        Inamaluwa<br />
-                        <i className="fa fa-phone fa-lg"></i>: +5451651651<br />
+                        Sigiriya Road,<br />
+                        Inamaluwa.<br />
+                        <i className="fa fa-phone fa-lg"></i>: +654656565646<br />
                         <i className="fa fa-fax fa-lg"></i>: +654656565646<br />
-                        <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
+                        <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:test@test.net">
                             test@test.net</a>
                         </address>
-                    </div>
-                    <div className="col-12 col-sm-4 align-self-center">
-                        <div className="text-center">
-                            <a className="btn btn-social-icon" href="mailto:"><i className="fa fa-envelope-o"></i></a>
-                            <p>E-mail</p>
-                        </div>
                     </div>
                 </div>
                 <div className="row justify-content-center">             
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input} from 'reactstrap';
+import {Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input} from 'reactstrap';
 import {NavLink} from 'react-router-dom';
 
 class Header extends Component {
