eb88ecbb3df820d538929d99380ea3982ece688e
s16417 <s16417@sci.pdn.ac.lk>
2020-07-23 10:40:08 +0530

    Adding firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDu7OqiFRmnYh_81Dcke6m9jxiUuqFjU8M",
+  authDomain: "shifted-timetables.firebaseapp.com",
+  databaseURL: "https://shifted-timetables.firebaseio.com",
+  projectId: "shifted-timetables",
+  storageBucket: "shifted-timetables.appspot.com",
+  messagingSenderId: "284988692648",
+  appId: "1:284988692648:web:3ddec0cce41eca1fe44a9d",
+  measurementId: "G-2LKBD9QF2C"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
   render() {
