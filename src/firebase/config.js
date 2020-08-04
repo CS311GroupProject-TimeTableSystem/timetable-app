a332f9e441f6e47e096b316592a2172f59506e69
kusal sirimanna <kusalsirimanna97@gmail.com>
2020-08-04 14:28:57 +0530

    after firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,3 +1,11 @@
 export const config = {
-  //add the firebase config here
+    //add the firebase config here
+    apiKey: "AIzaSyDu7OqiFRmnYh_81Dcke6m9jxiUuqFjU8M",
+    authDomain: "shifted-timetables.firebaseapp.com",
+    databaseURL: "https://shifted-timetables.firebaseio.com",
+    projectId: "shifted-timetables",
+    storageBucket: "shifted-timetables.appspot.com",
+    messagingSenderId: "284988692648",
+    appId: "1:284988692648:web:3ddec0cce41eca1fe44a9d",
+    measurementId: "G-2LKBD9QF2C"
 }
